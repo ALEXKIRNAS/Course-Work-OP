e5379e8f711b3cd4a5730828e167ad4fdc60bf4f
ALEXKIRNAS <alexkirnas@ukr.net>
2016-03-21 17:52:08 +0200

    Добавлена функція обчислення визначника

--- Course Work/Course Work/DetermineCalc.h
+++ Course Work/Course Work/DetermineCalc.h
@@ -1,1 +1,5 @@
 #pragma once
+int det(const int**, int); // Прототип функції обчислення визначника
+int AlgebralMinorCalc(const int const**, const int&, const int&); // Прототип функції обчислення алгебраїчного мінору заданого елементу
+int AlgebralAdditionCalc(const int const**, const int&, const int&); // Прототип функції обчислення алгебраїного доповнення заданого елементу
+int** matrixCut(const int**, const int&, const int&); // Прототип функції уворення матриці (N-1)-го порядку
