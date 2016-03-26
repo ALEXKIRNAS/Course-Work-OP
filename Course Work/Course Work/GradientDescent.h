3aba1e9ad571129594e3bfb46fe1400d2cd34ca1
ALEXKIRNAS <alexkirnas@ukr.net>
2016-03-26 18:57:34 +0200

    Добавлено критерій Сильвестра

---
+++ Course Work/Course Work/GradientDescent.h
@@ -0,0 +1,3 @@
+#pragma once
+bool isSemetric(double**, const int&); // Прототип функції перевірки матриці на симетричність
+bool isPositive(double**, const int&); // Прототип функції перевірки матриці на додатньовизначеність
